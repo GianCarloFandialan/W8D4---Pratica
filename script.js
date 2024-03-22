938facd95207a9a737e0f5f69676a7cfdbf13512
Gian Carlo Fandialan <gianchifandialan03@gmail.com>
2024-03-22 19:58:21 +0100

    primo caricamento

---
+++ script.js
@@ -0,0 +1,35 @@
+//2)Crea la possibilità di contare il numero di "viaggi" presenti sulla pagina.
+
+cards = document.getElementsByClassName("card");
+console.log(cards);
+
+function counter() {
+    counter = 0;
+    for (const card of cards) {
+        counter += 1
+    }
+    return counter
+}
+
+console.log(counter());
+
+//4)Quando l'utente cliccherà su "Contattaci", fai in modo che il modale venga visualizzato.
+
+const contattaciModale = document.getElementById('contattaci-modale')
+if (contattaciModale) {
+  contattaciModale.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = contattaciModale.querySelector('.modal-title')
+    const modalBodyInput = contattaciModale.querySelector('.modal-body input')
+
+    modalTitle.textContent = `Contattaci via email`
+    modalBodyInput.value = recipient
+  })
+}
